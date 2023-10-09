8065ea666ec7edb5d5088c1f1a8efd81bc2632a2
sergey <sergey.listratov@gmail.com>
2023-10-09 23:12:43 +0300

    Edu_part_01_1

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 
 from app.bookings.dao import BookingDAO
 from app.bookings.schemas import SBooking
@@ -10,8 +10,12 @@ router = APIRouter(
 
 
 @router.get("")
-async def get_bookings() -> list[SBooking]:
-    return await BookingDAO.find_all()
+async def get_bookings(request: Request): #-> list[SBooking]:
+    print(request.cookies)
+    print(request.url)
+    print(request.client)
+    return request.cookies
+    # return await BookingDAO.find_all()
 
 
 # @router.get("")
